7bdd8f1f4d0b3859bece06a898439ef95609bb74
sagar-prajapat <154601734+sagar-prajapat@users.noreply.github.com>
2024-06-05 10:12:35 +0530

    Add files via upload

---
+++ Height_tree.java
@@ -0,0 +1,34 @@
+public class Height_tree {
+    static class Node{
+        int data;
+        Node left,right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+
+        int leftheight = height(root.left);
+        int rightheight = height(root.right);
+
+        return Math.max(leftheight, rightheight)+1;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(height(root));
+    }
+}
---
+++ sum_nodes.java
@@ -0,0 +1,32 @@
+public class sum_nodes {
+    static class Node{
+        int data;
+        Node left,right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int sum(Node root){
+        if(root==null){
+            return 0;
+        }
+
+        int leftsum = sum(root.left);
+        int rightsum = sum(root.right);
+        return leftsum + rightsum + root.data;
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+        System.out.println(sum(root));
+    }
+}
